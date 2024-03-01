ac9eeef9d978415c263abec102a83bcd65305836
UnProMan <zero3771@gmail.com>
2024-03-01 20:22:09 +0900

    url 약간 변경(user -> users)

--- src/api/user.ts
+++ src/api/user.ts
@@ -8,16 +8,16 @@ import type {
 import axiosInstance from "./config.ts";
 
 export const loginUser = async ({ queryParams: params }: LoginRequest) =>
-    await axiosInstance.post<User>("/user/login", params);
+    await axiosInstance.post<User>("/users/login", params);
 
 export const logoutUser = async () =>
-    await axiosInstance.post("/user/logout");
+    await axiosInstance.post("/users/logout");
 
 export const refreshData = async () =>
-    await axiosInstance.get<User>("/user/refreshData");
+    await axiosInstance.get<User>("/users/refreshData");
 
 export const joinUser = async (req: JoinUserRequest) =>
     await axiosInstance.post("/join", req);
 
 export const personalStatusUsers = async ({ queryParams: params }: PersonalStatusRequest) =>
-    await axiosInstance.get<PersonalStatusUsers>('/user/personal/status', {params});
+    await axiosInstance.get<PersonalStatusUsers>('/users/personal/status', {params});
